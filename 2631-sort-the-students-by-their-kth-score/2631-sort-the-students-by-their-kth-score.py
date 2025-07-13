0ab7bafb5be40b95e96f0bf840baacf9daf39556
hayat123-nur <nurahayat4@gmail.com>
2025-07-13 06:47:03 +0300

    Time: 4 ms (46.32%), Space: 19.4 MB (48.42%) - LeetHub

---
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def sortTheStudents(self, score, k):
+        """
+        :type score: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return sorted(score, key=lambda x:x[k],reverse=True)
