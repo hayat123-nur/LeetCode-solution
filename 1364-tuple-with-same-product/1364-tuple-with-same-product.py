f112f7821ced5abc23fd9d19ebaf17364d24384b
hayat123-nur <nurahayat4@gmail.com>
2025-07-26 22:11:09 +0300

    Time: 347 ms (72.56%), Space: 46.5 MB (28.31%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -4,8 +4,8 @@ class Solution:
         count=defaultdict(int)
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                prod=nums[i]*nums[j]
-                count[prod]+=1
+                product=nums[i]*nums[j]
+                count[product]+=1
         result=0
         for c in count.values():
             if c>1:
