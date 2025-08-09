46a14a585072981ddc9e0177b442faa71cef86b7
hayat123-nur <nurahayat4@gmail.com>
2025-08-09 09:33:42 +0300

    Time: 71 ms (16.19%), Space: 17.8 MB (73.99%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def palindrome(i,j):
+            while i<j:
+                if s[i]!=s[j]:
+                    return False
+                i+=1
+                j-=1
+            return True
+        i,j=0,len(s)-1
+        while i<j:
+            if s[i]!=s[j]:
+                return palindrome(i+1,j) or palindrome(i,j-1)
+            i+=1
+            j-=1
+        return True        
+               
+                
+        
