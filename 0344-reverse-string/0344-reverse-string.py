a93f06cc6446ed036a96f0ec58fdd2ded820ae2e
hayat123-nur <nurahayat4@gmail.com>
2025-10-10 19:33:03 +0300

    Time: 0 ms (100%), Space: 23.1 MB (42.42%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def reverseString(self, s):
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
         """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
+        Do not return anything, modify s in-place instead.
         """
         s.reverse()
         
